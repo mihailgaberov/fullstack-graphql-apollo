74308e38cd92ce15af73af964f2fba260a8fc2fb
mihailgaberov <mihail.gaberov@gmail.com>
2023-12-06 12:21:16 +0100

    :sparkles: added track API methods to fetch tracks data via RESTDataSource class

---
+++ server/src/datasources/track-api.js
@@ -0,0 +1,21 @@
+const { RESTDataSource } = require("@apollo/datasource-rest");
+
+class TrackAPI extends RESTDataSource {
+  baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
+
+  getTracksForHome() {
+    return this.get("tracks");
+  }
+
+  getAuthor(authorId) {
+    return this.get(`author/${encodeURIComponent(authorId)}`);
+  }
+
+  getTrack(trackId) {
+    return this.get(`track/${encodeURIComponent(trackId)}`);
+  }
+
+  getTrackModules(trackId) {
+    return this.get(`track/${encodeURIComponent(trackId)}/modules`);
+  }
+}
