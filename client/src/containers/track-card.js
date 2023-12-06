413396976c89c15dcdb28d0bf129d5cf4645a495
mihailgaberov <mihail.gaberov@gmail.com>
2023-12-06 15:00:06 +0100

    :sparkles: use mutation from the client

--- client/src/containers/track-card.js
+++ client/src/containers/track-card.js
@@ -3,6 +3,21 @@ import styled from '@emotion/styled';
 import { colors, mq } from '../styles';
 import { humanReadableTimeFromSeconds } from '../utils/helpers';
 import { Link } from "react-router-dom";
+import { gql, useMutation} from "@apollo/client";
+
+const INCREMENT_TRACK_VIEWS = gql`
+  mutation IncrementTrackViewsMutation($incrementTrackViewsId: ID!) {
+    incrementTrackViews(id: $incrementTrackViewsId) {
+      code
+      success
+      message
+      track {
+        id
+        numberOfViews
+      }
+    }
+  }
+`;
 
 /**
  * Track Card component renders basic info in a card format
@@ -10,9 +25,18 @@ import { Link } from "react-router-dom";
  */
 const TrackCard = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track;
+  const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
+      variables: { incrementTrackViewsId: id },
+    onCompleted: data => {
+      console.log(data);
+    },
+    onError: error => {
+      console.log(error);
+    }
+  })
 
   return (
-    <CardContainer to={`/track/${id}`}>
+    <CardContainer to={`/track/${id}`} onClick={incrementTrackViews}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail} alt={title} />
