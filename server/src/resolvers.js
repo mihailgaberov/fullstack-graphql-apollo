700b5cce6383c9ff30848a10d24effd20ca7fb51
mihailgaberov <mihail.gaberov@gmail.com>
2023-12-06 13:44:23 +0100

    :sparkles: add track resolver

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -3,6 +3,10 @@ const resolvers = {
     // returns an array of Tracks that will be used to populate the homepage grid of our web client
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome()
+    },
+    // returns a single Track that will be used to populate the Track detail page of our web client
+    track: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getTrack(id)
     }
   },
   Track: {
